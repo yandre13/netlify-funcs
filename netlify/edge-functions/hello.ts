5ea5dc7e900a3d2975d5c4257a723cdc38b3ccad
yandre13 <yandre13.elsf@gmail.com>
2023-04-04 17:58:35 -0500

    comment edge

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,24 +1,24 @@
-import { Config, Context } from 'https://edge.netlify.com'
+// import { Config, Context } from 'https://edge.netlify.com'
 
-const handler = async (request: Request, context: Context) => {
-  const message = await fetch(
-    'http://localhost:8888/.netlify/functions/hello',
-    {
-      headers: {
-        Accept: 'application/json',
-      },
-    }
-  )
+// const handler = async (request: Request, context: Context) => {
+//   const message = await fetch(
+//     'http://localhost:8888/.netlify/functions/hello',
+//     {
+//       headers: {
+//         Accept: 'application/json',
+//       },
+//     }
+//   )
 
-  const jsonData = await message.json()
-  return new Response(jsonData.message, {
-    headers: {
-      'content-type': 'text/plain;charset=UTF-8',
-    },
-  })
-}
-export default handler
+//   const jsonData = await message.json()
+//   return new Response(jsonData.message, {
+//     headers: {
+//       'content-type': 'text/plain;charset=UTF-8',
+//     },
+//   })
+// }
+// export default handler
 
-export const config: Config = {
-  path: '/edge-functions/hello',
-}
+// export const config: Config = {
+//   path: '/edge-functions/hello',
+// }
